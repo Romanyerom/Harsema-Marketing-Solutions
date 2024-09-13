e528061b127e5996306149479d77390e737ae371
Romanyerom <Romanyerom@github.com>
2024-09-13 09:50:01 +0000

    Updates

---
+++ precache-manifest.ae475793c1b40b3bdbb156c5f1876908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dcb9268307d6f998e7caf2e850c8edf",
+    "url": "/harsema-marketing-solutions/index.html"
+  },
+  {
+    "revision": "61d35c70d73f4421e529",
+    "url": "/harsema-marketing-solutions/static/css/main.177965d3.chunk.css"
+  },
+  {
+    "revision": "e346ed6b809c1f1a6cc6",
+    "url": "/harsema-marketing-solutions/static/js/2.9809098b.chunk.js"
+  },
+  {
+    "revision": "61d35c70d73f4421e529",
+    "url": "/harsema-marketing-solutions/static/js/main.3f044d08.chunk.js"
+  },
+  {
+    "revision": "bd3529ad6fca0a50f752",
+    "url": "/harsema-marketing-solutions/static/js/runtime~main.6bae5cdd.js"
+  }
+]);
