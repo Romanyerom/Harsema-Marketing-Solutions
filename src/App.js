a293fcb5b994868474f2aa4079993af376eee083
Romanyerom <Romanyerom@github.com>
2024-09-16 10:56:08 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';  // Import the NotFound component
+import Subscription from './pages/Subscription';
+import NotFound from './pages/NotFound';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
+      <div>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/services">Services</a>
+          <a href="/contact">Contact</a>
+          <a href="/subscribe">Subscribe</a>
+        </nav>
 
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* NotFound route to handle non-existent pages */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/subscribe" component={Subscription} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
 
