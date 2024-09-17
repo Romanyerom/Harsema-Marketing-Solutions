55d2747e3cf5c9fb7170fbde01c2fd07864bbdf6
Romanyerom <Romanyerom@github.com>
2024-09-17 16:22:40 +0000

    Completed build and added footer with copyright

---
+++ build/precache-manifest.55b0294b798f70a92ccfc167c011508e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d9c04c5a2cab59772d1cdcf9ecb0d1",
+    "url": "/harsema-marketing-solutions/index.html"
+  },
+  {
+    "revision": "5a10d6a83ebce43d598a",
+    "url": "/harsema-marketing-solutions/static/css/main.177965d3.chunk.css"
+  },
+  {
+    "revision": "413ae00e917d16be7a5a",
+    "url": "/harsema-marketing-solutions/static/js/2.770ec9a8.chunk.js"
+  },
+  {
+    "revision": "5a10d6a83ebce43d598a",
+    "url": "/harsema-marketing-solutions/static/js/main.eddff6f3.chunk.js"
+  },
+  {
+    "revision": "bd3529ad6fca0a50f752",
+    "url": "/harsema-marketing-solutions/static/js/runtime~main.6bae5cdd.js"
+  }
+]);
