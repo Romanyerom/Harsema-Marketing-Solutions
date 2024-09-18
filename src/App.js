f3df6d6857ad32a2aeae90deafa4307bba1908d0
Romanyerom <Romanyerom@github.com>
2024-09-18 16:52:58 +0000

    updating pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -13,32 +13,21 @@ const App = () => {
   return (
     <Router>
       <div style={styles.container}>
-        {/* Include the Header component */}
+        {/* Include Header which already contains the logo and navigation */}
         <Header />
 
-        {/* Navigation Links within the Header (use of <Link> to navigate) */}
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/subscribe">Subscribe</Link></li>
-          </ul>
-        </nav>
-
-        {/* Define Routes for each page */}
+        {/* Define Routes for different pages */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/subscribe" element={<Subscription />} />
-          {/* Catch-all route for 404 */}
+          {/* Catch-all route for unmatched URLs */}
           <Route path="*" element={<NotFound />} />
         </Routes>
 
-        {/* Footer Component */}
+        {/* Include Footer */}
         <Footer />
       </div>
     </Router>
